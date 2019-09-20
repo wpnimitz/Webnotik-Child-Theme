2ba2acfbae014562c1d4f7956dd1d276e1a194c0
wpnimitz <wpnimitz@gmail.com>
2019-09-20 19:49:39 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,76 +1,42 @@
 <?php
+include_once("plugins/webnotik.php");
+
 add_action( 'wp_enqueue_scripts', 'custom_assets' );
 function custom_assets() {
 	$ver = "1.0.1" . strtotime("now");
     wp_enqueue_style( 'app-style', get_stylesheet_directory_uri() . '/assets/css/app-style.css', '', $ver );
 }
 
-function autopilot_seller_form( $atts ){  
+function webnotik_form_shortcode( $atts ){  
+	$atts = shortcode_atts(
+		array(
+			'type' => 'seller',
+		), $atts, 'webnotik_form' );
+	$type = $atts["type"];
 
-	$ret = '';
-	$ret .= '<form action="https://qfautopilot.com/form.php?userid=15051&redirect=https://rehubgroup.com/seller-thank-you/" class="gform_wrapper custom_form" method="post">';
-	$ret .= '<input id="input_form_group_id" name="input_form_group_id" type="hidden" value="4983">';
-	$ret .= '<input name="206f6a7ada917912e9389da75d80be3b" type="hidden" value="">';
-	$ret .= '<input name="embed" type="hidden" value="1">';
-	$ret .= '<input id="input_form_campaign_id" name="input_form_campaign_id" type="hidden" value="18040">';
-	$ret .= '<input name="user-uid" type="hidden" value="15051">';
-	$ret .= '<input name="add-contact_first_name" placeholder="First Name" required="" style="margin-bottom: 8px;" type="text">';
-	$ret .= '<input name="add-contact_last_name" placeholder="Last Name" required="" style="margin-bottom: 8px;" type="text">';
-	$ret .= '<input name="add-contact_email" placeholder="Email Address" required="" style="margin-bottom: 8px;" type="text">';
-	$ret .= '<input name="add-contact_phone" placeholder="Phone Number" required="" style="margin-bottom: 8px;" type="text">';
-		$ret .= '<input type="submit" value="Submit">';
-	$ret .= '</form>';
+	$allowed_types = array('seller', 'buyer', 'other', 'extra1', 'extra2' );
 
-	return $ret;
-}
-add_shortcode( 'autopilot_seller', 'autopilot_seller_form' );
-
-
-function autopilot_investor_form( $atts ){  
-
-	$ret = '';
-	$ret .= '<form action="https://qfautopilot.com/form.php?userid=15051&redirect=https://rehubgroup.com/investor-thank-you/" class="gform_wrapper custom_form" method="post">';
-	$ret .= '<input id="input_form_group_id" name="input_form_group_id" type="hidden" value="4981">';
-	$ret .= '<input name="206f6a7ada917912e9389da75d80be3b" type="hidden" value="">';
-	$ret .= '<input name="embed" type="hidden" value="1">';
-	$ret .= '<input id="input_form_campaign_id" name="input_form_campaign_id" type="hidden" value="18041">';
-	$ret .= '<input name="user-uid" type="hidden" value="15051">';
-	$ret .= '<input name="add-contact_first_name" placeholder="First Name" required="" style="margin-bottom: 8px;" type="text">';
-	$ret .= '<input name="add-contact_last_name" placeholder="Last Name" required="" style="margin-bottom: 8px;" type="text">';
-	$ret .= '<input name="add-contact_email" placeholder="Email Address" required="" style="margin-bottom: 8px;" type="text">';
-	$ret .= '<input name="add-contact_phone" placeholder="Phone Number" required="" style="margin-bottom: 8px;" type="text">';
-		$ret .= '<input type="submit" value="Submit">';
-	$ret .= '</form>';
+	if(in_array($type, $allowed_types)) {
+		$form = get_option( 'webnotik_' . $type . '_form');
+		if($form != "") {
+			$ret = $form;
+		} else {
+			$ret = "Form is empty!";
+		}
+	} else {
+		$ret = 'Not allowed types';
+	}
 
 	return $ret;
+
+	
 }
-add_shortcode( 'autopilot_investor', 'autopilot_investor_form' );
+add_shortcode( 'webnotik_form', 'webnotik_form_shortcode' );
 
 
 
-function autopilot_buyer_form( $atts ) {
 
-	$ret = '';
-	$ret .= '<form class="gform_wrapper custom_form" action="https://qfautopilot.com/form.php?userid=15051&redirect=https://rehubgroup.com/buyer-thank-you/" class="custom_form " method="post"> ';
-	$ret .= '<input type="hidden" name="input_form_group_id" id="input_form_group_id" value="4982" />';
-	$ret .= '<input type="hidden" name="206f6a7ada917912e9389da75d80be3b" value="" />';
-	$ret .= '<input type="hidden" name="embed" value="1" />';
-	$ret .= '<input type="hidden" name="input_form_campaign_id" id="input_form_campaign_id" value="18042" />';
-	$ret .= '<input type="hidden" name="user-uid" value="15051">';
 
-	$ret .= '<input type="text" name="add-contact_first_name" style="margin-bottom: 8px;" placeholder="First name" required><br>';
-
-	$ret .= '<input type="text" name="add-contact_last_name" style="margin-bottom: 8px;" placeholder="Last Name" required><br>';
-
-	$ret .= '<input type="text" name="add-contact_email" style="margin-bottom: 8px;" placeholder="Email Address" required><br>';
-
-	$ret .= '<input type="text" name="add-contact_phone" style="margin-bottom: 8px;" placeholder="Phone" required><br>';
-		$ret .= '<input type="submit" value="Submit" >';
-	$ret .= '</form>';
-
-	return $ret;
-}
-add_shortcode( 'autopilot_buyer', 'autopilot_buyer_form' );
 
 
 
