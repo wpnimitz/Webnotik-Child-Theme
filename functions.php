13eb701dc98a7d362057cf6f83671efe6dd118d4
wpnimitz <wpnimitz@gmail.com>
2019-09-29 00:45:37 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ function webnotik_form_shortcode( $atts ){
 	if(in_array($type, $allowed_types)) {
 		$form = get_option( 'webnotik_' . $type . '_form');
 		if($form != "") {
-			$ret = '<div class="gform_wrapper webnotik-'.$type.'">'. $form . '</div>';
+			$ret = '<div class="gform_wrapper webnotik-'.$type.'">'. do_shortcode($form) . '</div>';
 		} else {
 			$ret = "Form is empty!";
 		}
