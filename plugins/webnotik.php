e0ff1f9fdc7667a7428545ce16b4bd89a8d5459b
wpnimitz <wpnimitz@gmail.com>
2019-09-30 19:36:12 +0800

    Update webnotik.php

--- plugins/webnotik.php
+++ plugins/webnotik.php
@@ -54,7 +54,7 @@ function webnotik_real_estate_content(){
 				<a class="forms-group <?php echo $tab == "forms" ? 'active' : 'inactive'; ?>" href="admin.php?page=webnotik-real-estate&tab=forms">Forms</a>
 				<a class="forms-group <?php echo $tab == "keywords" ? 'active' : 'inactive'; ?>" href="admin.php?page=webnotik-real-estate&tab=keywords">Keywords</a>				
 				<a class="forms-group <?php echo $tab == "divi-global" ? 'active' : 'inactive'; ?>" href="admin.php?page=webnotik-real-estate&tab=divi-global">Divi Global</a>				
-				<a class="forms-group <?php echo $tab == "others" ? 'active' : 'inactive'; ?>" href="admin.php?page=webnotik-real-estate&tab=others">Others</a>
+				<a class="forms-group <?php echo $tab == "help" ? 'active' : 'inactive'; ?>" href="admin.php?page=webnotik-real-estate&tab=help">Help</a>
 				<a href="#" class="icon">&#9776;</a>			
 			</div>
 		</div>
