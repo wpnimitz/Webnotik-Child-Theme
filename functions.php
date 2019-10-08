eb1342e38c730cc79a9cdd9522acc6d38c8460ce
wpnimitz <wpnimitz@gmail.com>
2019-10-08 10:24:47 +0800

    Remove function main_content

--- functions.php
+++ functions.php
@@ -7,15 +7,6 @@ function custom_assets() {
     wp_enqueue_style( 'app-style', get_stylesheet_directory_uri() . '/assets/css/app-style.css', '', $ver );
 }
 
-
-add_action( 'et_before_main_content', 'webnotik_global_header' );
-function webnotik_global_header() {
-	if(is_category(6)) {
-		echo do_shortcode('<div class="webnotik-wrapper">[et_pb_section global_module="924"][/et_pb_section]</div>');
-	}
-    
-}
-
 add_filter( 'body_class', 'webnotik_body_class' );
 function webnotik_body_class( $classes ) {
 	if(is_page()) {
