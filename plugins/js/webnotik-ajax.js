b30d6fdb5985fc5670e819ed3aab2dbe850549ce
wpnimitz <wpnimitz@gmail.com>
2019-10-08 12:26:47 +0800

    ajax

---
+++ plugins/js/webnotik-ajax.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function( $ ) {	
+	$("#get-cp").on("click", function(e) {
+		e.preventDefault();
+		var data = {
+            action: 'get_city_pages',
+            focus_key: 'We Buy Houses'
+        };
+
+        $.getJSON( get_city_pages_data.ajaxurl, data, function( json ) {
+            if ( json.success ) {
+                console.log('yes!')
+            }
+        } );
+
+
+		// var data = {
+	 //        action: 'get_city_pages',
+	 //        focus_key: 'We Buy Houses'
+	 //    };
+
+	 //    jQuery.post(ajaxurl, data, function(response) {
+	 //        json = $.parseJSON(response);
+	 //        console.log('Got this from the server: ' + response);
+	 //        $.each(response, function(i, item) {
+	 //        	$(".add-sub-keyword").trigger("click");
+	 //        	$("#extra-" + extraSub + " input").attr('value', item.PageName);
+		// 	});
+	 //    });
+
+		
+	})
+});
