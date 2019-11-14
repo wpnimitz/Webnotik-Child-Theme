c3c93d0ef5d03b599ca6b2b2d59e6313551b2839
wpnimitz <wpnimitz@gmail.com>
2019-11-15 02:39:22 +0800

    Update header-style-1.php

--- templates/header-style-1.php
+++ templates/header-style-1.php
@@ -1,5 +1,4 @@
-	
-	<div class="header-style-1">
+	<div class="webnotik-header header-style-1">
 		<div class="container">
 			<div class="header-menu">
 				<div id="et-top-navigation" data-height="<?php echo esc_attr( et_get_option( 'menu_height', '66' ) ); ?>" data-fixed-height="<?php echo esc_attr( et_get_option( 'minimized_menu_height', '40' ) ); ?>">
