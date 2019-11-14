7647ae2a70b63795d53b534b7f01b292460bc939
wpnimitz <wpnimitz@gmail.com>
2019-11-15 02:34:22 +0800

    header 1 testing start

---
+++ templates/header-style1.php
@@ -0,0 +1,99 @@
+		<header id="main-header" data-height-onload="<?php echo esc_attr( et_get_option( 'menu_height', '66' ) ); ?>">
+			<div class="container clearfix et_menu_container">
+			<?php
+				$logo = ( $user_logo = et_get_option( 'divi_logo' ) ) && ! empty( $user_logo )
+					? $user_logo
+					: get_stylesheet_directory_uri() . '/assets/img/rei-toolbox.jpg';
+
+				ob_start();
+			?>
+				<div class="logo_container">
+					<span class="logo_helper"></span>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<img src="<?php echo esc_attr( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" id="logo" data-height-percentage="<?php echo esc_attr( et_get_option( 'logo_height', '54' ) ); ?>" />
+					</a>
+				</div>
+			<?php
+				$logo_container = ob_get_clean();
+
+				/**
+				 * Filters the HTML output for the logo container.
+				 *
+				 * @since 3.10
+				 *
+				 * @param string $logo_container
+				 */
+				echo et_core_intentionally_unescaped( apply_filters( 'et_html_logo_container', $logo_container ), 'html' );
+			?>
+				<div id="et-top-navigation" data-height="<?php echo esc_attr( et_get_option( 'menu_height', '66' ) ); ?>" data-fixed-height="<?php echo esc_attr( et_get_option( 'minimized_menu_height', '40' ) ); ?>">
+					<?php if ( ! $et_slide_header || is_customize_preview() ) : ?>
+						<nav id="top-menu-nav">
+						<?php
+							$menuClass = 'nav';
+							if ( 'on' === et_get_option( 'divi_disable_toptier' ) ) $menuClass .= ' et_disable_top_tier';
+							$primaryNav = '';
+
+							$primaryNav = wp_nav_menu( array( 'theme_location' => 'primary-menu', 'container' => '', 'fallback_cb' => '', 'menu_class' => $menuClass, 'menu_id' => 'top-menu', 'echo' => false ) );
+							if ( empty( $primaryNav ) ) :
+						?>
+							<ul id="top-menu" class="<?php echo esc_attr( $menuClass ); ?>">
+								<?php if ( 'on' === et_get_option( 'divi_home_link' ) ) { ?>
+									<li <?php if ( is_home() ) echo( 'class="current_page_item"' ); ?>><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Home', 'Divi' ); ?></a></li>
+								<?php }; ?>
+
+								<?php show_page_menu( $menuClass, false, false ); ?>
+								<?php show_categories_menu( $menuClass, false ); ?>
+							</ul>
+						<?php
+							else :
+								echo et_core_esc_wp( $primaryNav );
+							endif;
+						?>
+						</nav>
+					<?php endif; ?>
+
+					<?php
+					if ( ! $et_top_info_defined && ( ! $et_slide_header || is_customize_preview() ) ) {
+						et_show_cart_total( array(
+							'no_text' => true,
+						) );
+					}
+					?>
+
+					<?php if ( $et_slide_header || is_customize_preview() ) : ?>
+						<span class="mobile_menu_bar et_pb_header_toggle et_toggle_<?php echo esc_attr( et_get_option( 'header_style', 'left' ) ); ?>_menu"></span>
+					<?php endif; ?>
+
+					<?php if ( ( false !== et_get_option( 'show_search_icon', true ) && ! $et_slide_header ) || is_customize_preview() ) : ?>
+					<div id="et_top_search">
+						<span id="et_search_icon"></span>
+					</div>
+					<?php endif; // true === et_get_option( 'show_search_icon', false ) ?>
+
+					<?php
+
+					/**
+					 * Fires at the end of the 'et-top-navigation' element, just before its closing tag.
+					 *
+					 * @since 1.0
+					 */
+					do_action( 'et_header_top' );
+
+					?>
+				</div> <!-- #et-top-navigation -->
+			</div> <!-- .container -->
+			<div class="et_search_outer">
+				<div class="container et_search_form_container">
+					<form role="search" method="get" class="et-search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<?php
+						printf( '<input type="search" class="et-search-field" placeholder="%1$s" value="%2$s" name="s" title="%3$s" />',
+							esc_attr__( 'Search &hellip;', 'Divi' ),
+							get_search_query(),
+							esc_attr__( 'Search for:', 'Divi' )
+						);
+					?>
+					</form>
+					<span class="et_close_search_field"></span>
+				</div>
+			</div>
+		</header> <!-- #main-header -->
