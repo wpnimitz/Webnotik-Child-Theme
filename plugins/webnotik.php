f50cff8f8a8f6e4d83fd67e672b8e62021a07cbb
wpnimitz <wpnimitz@gmail.com>
2019-11-15 23:07:19 +0800

    Update webnotik.php

--- plugins/webnotik.php
+++ plugins/webnotik.php
@@ -401,7 +401,8 @@ function webnotik_real_estate_content(){
 					    	<label for="wda_form['hero_header']">Form Header</label>
 					    </div>
 				    	<div class="form-field">
-				    		<input type="text" name="wda_form['hero_header']"  data-default-color="<?php echo $wda_form['hero_header']; ?>" id="wda_form['hero_header']" class="wda_color_picker" value="<?php echo $wda_form['hero_header']; ?>">    	
+				    		<input type="hidden" name="wda_form['hero_header']" value="<?php echo $wda_form['hero_header']; ?>" class="color_holder">
+				    		<input type="text" data-default-color="<?php echo $wda_form['hero_header']; ?>" id="wda_form['hero_header']" class="wda_color_picker" value="<?php echo $wda_form['hero_header']; ?>">    	
 					    </div>
 				    </div>
 				    <div class="form-group">
@@ -412,6 +413,15 @@ function webnotik_real_estate_content(){
 				    		<input type="hidden" name="wda_form['hero_body']" value="<?php echo $wda_form['hero_body']; ?>" class="color_holder">
 
 				    		<input type="text" data-default-color="<?php echo $wda_form['hero_header']; ?>" id="wda_form['hero_body']" class="wda_color_picker" value="<?php echo $wda_form['hero_body']; ?>">    	
+					    </div>
+				    </div>		
+
+				    <div class="form-group">
+				    	<div class="form-label">
+					    	<label for="wda_form['test']">Form Body</label>
+					    </div>
+				    	<div class="form-field">
+				    		<input type="text" data-default-color="<?php echo $wda_form['test']; ?>" id="wda_form['test']" class="" value="<?php echo $wda_form['test']; ?>">    	
 					    </div>
 				    </div>				    
 				    <?php submit_button(); ?>
