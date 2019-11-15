b0a8a917ccb1b8ffec4fdf1815827dd90e6e6dd2
wpnimitz <wpnimitz@gmail.com>
2019-11-15 08:52:05 +0800

    Update webnotik.php

--- plugins/webnotik.php
+++ plugins/webnotik.php
@@ -377,12 +377,12 @@ function webnotik_real_estate_content(){
 
 				    <div class="form-group">
 				    	<div class="form-label">
-					    	<label for="webnotik_main_topics">Header Style</label>
+					    	<label for="webnotik_header_style">Header Style</label>
 					    </div>
 				    	<div class="form-field">
 
-				    		<select name="webnotik_main_topics" id="webnotik_main_topics">
-				    		<?php $header_style =  get_option('webnotik_main_topics'); ?>
+				    		<select name="webnotik_header_style" id="webnotik_header_style">
+				    		<?php $header_style =  get_option('webnotik_header_style'); ?>
 				    		<option value="normal" <?php echo $header_style == "normal" ? 'selected' : '' ?>>Normal Header</option>
 				    		<option value="style-1" <?php echo $header_style == "style-1" ? 'selected' : '' ?>>Style 1</option>
 				    		</select>
