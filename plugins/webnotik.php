7ef9ffb8db81b490fce37ee76f555fca3524b50e
wpnimitz <wpnimitz@gmail.com>
2019-11-23 07:55:33 +0800

    Update webnotik.php

--- plugins/webnotik.php
+++ plugins/webnotik.php
@@ -1217,9 +1217,7 @@ function webnotik_divi_global_footer() {
 add_action( 'wp_ajax_generate_new_rei_style', 'generate_new_rei_style' );
 function generate_new_rei_style() {
 	$file = get_stylesheet_directory() . '/assets/css/rei-style.css';
-	$current = file_get_contents('includes/style.php');
+	$current = file_get_contents(get_stylesheet_directory() . '/includes/style.php');
 	file_put_contents($file, $current);
-
-	echo 'test';
 	wp_send_json_success($current);
 }
