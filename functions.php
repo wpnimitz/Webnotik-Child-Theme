69ee011f94a6ae7c705cf1d4bac8cb2cea3b0349
wpnimitz <wpnimitz@gmail.com>
2019-11-23 07:24:26 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ add_action( 'wp_enqueue_scripts', 'custom_assets' );
 function custom_assets() {
 	$ver = "1.0.1" . strtotime("now");
     wp_enqueue_style( 'app-style', get_stylesheet_directory_uri() . '/assets/css/app-style.css', '', $ver );
-    wp_enqueue_style( 'rei-style', get_stylesheet_directory_uri() . '/includes/style.php', '', $ver );
+    wp_enqueue_style( 'rei-style', get_stylesheet_directory_uri() . '/assets/css/rei-style.css', '', $ver );
 }
 
 add_filter( 'body_class', 'webnotik_body_class' );
@@ -27,3 +27,15 @@ function webnotik_body_class( $classes ) {
 
     return $classes;
 }
+
+
+/**
+ * Hook into options page after save.
+ */
+public function generate_new_rei_style( $old_value, $new_value ) {
+	$style = get_stylesheet_directory() . '/assets/css/rei-style.css';
+	$file = file_get_contents('includes/style.php');
+	file_put_contents($style, $file);
+}
+add_action( 'update_option_webnotik_register_branding_settings', 'generate_new_rei_style', 10, 2 );
+
