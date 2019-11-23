6b9a468512a036ec6d5037424ccab8308877d0c3
wpnimitz <wpnimitz@gmail.com>
2019-11-23 08:17:01 +0800

    Update style.php

--- includes/style.php
+++ includes/style.php
@@ -10,7 +10,6 @@ extract($wda_form);
 
 $css = '.webnotik-pages .form-header-hero  {
     background: '.$hero_header.';
-    border-bottom: 5px solid #fff;
 }';
 
 $css .= '.webnotik-pages .form-body-hero {
@@ -19,7 +18,6 @@ $css .= '.webnotik-pages .form-body-hero {
 
 $css .= '.webnotik-pages .form-header-content, .single .form-header-content {
     background: '.$content_header.';
-    border-bottom: 5px solid #fff;
 }';
 
 
