22b8555290ce0fad8d752128f916d7f42065355d
wpnimitz <wpnimitz@gmail.com>
2019-12-18 16:46:10 +0800

    Update style.php

--- includes/style.php
+++ includes/style.php
@@ -14,34 +14,39 @@ $border_radius = '5px';
 if(empty($hero_background)) {
 	$hero_background = get_stylesheet_directory_uri() . '/assets/img/no-bg.jpg';
 }
-$css .= '.webnotik-pages .hero-background {
+$css .= '
+.webnotik-pages .hero-background {
     background-image: linear-gradient(90deg,rgba(30,30,30,0.50) 30%,rgba(30,30,30,0.20) 75%),url('.$hero_background.');
 }';
 
 
 
 if(!empty($form_header)) {
-	$css .= '.webnotik-pages .form-header-hero,
+	$css .= '
+	.webnotik-pages .form-header-hero,
 	.webnotik-pages .form-hero .form-header,
 	.single .form-header-hero .form-header,
 	.single .form-hero .form-header  {
 	    background: '.$form_header.';
 	    
 	}';
-	$css .= '.webnotik-pages .et_pb_divider:before {
+	$css .= '
+	.webnotik-pages .et_pb_divider:before {
 	    border-top-color: '.$form_header.';
 	}';
 }
 
 if($remove_form_header_padding_bottom == "Yes") {
-	$css .= '.form-header {
+	$css .= '
+	.form-header {
 	    padding-bottom: 0 !important;
 	}';
 }
 
 
 if(!empty($form_body)) {
-	$css .= '.webnotik-pages .form-body-hero,
+	$css .= '
+	.webnotik-pages .form-body-hero,
 	.webnotik-pages .form-hero .form-body,
 	.single .form-header-hero .gform_wrapper,
 	.form-header-hero .gform_wrapper {
@@ -52,7 +57,8 @@ if(!empty($form_body)) {
 
 //depreciated since 1.2.7
 if(!empty($form_body)) {
-	$css .= '.webnotik-pages .form-header-content,
+	$css .= '
+	.webnotik-pages .form-header-content,
 	.single .form-header-content {
 	    background: '.$form_body.';
 	}';
@@ -61,20 +67,23 @@ if(!empty($form_body)) {
 
 //depreciated since 1.2.7
 if(!empty($form_body)) {
-	$css .= '.webnotik-pages .form-body-content, .single .form-body-content {
+	$css .= '
+	.webnotik-pages .form-body-content, .single .form-body-content {
 	    background: '.$form_body.';
 	}';
 }
 
 if($form_fields_size == "Small") {
-	$css .= '.et_pb_module .gform_wrapper input, .et_pb_module .gform_wrapper select, .et_pb_module .gform_wrapper textarea {
+	$css .= '
+	.et_pb_module .gform_wrapper input, .et_pb_module .gform_wrapper select, .et_pb_module .gform_wrapper textarea {
 		padding: 10px 15px !important;
 	}';
 }
 
 
 if(!empty($button_color)) {
-	$css .= '.et_pb_module *[type=submit], 
+	$css .= '
+	.et_pb_module *[type=submit], 
 	.et_pb_module .gform_wrapper button {
 		background: '.$button_color.';
 		margin: 0 auto;
@@ -88,13 +97,15 @@ if(!empty($button_color)) {
 		margin-bottom: 0;
 	}';
 
-	$css .= '.et_pb_module *[type=submit]:hover, 
+	$css .= '
+	.et_pb_module *[type=submit]:hover, 
 	.et_pb_module .gform_wrapper button:hover {
 	  background-position: 0 0;
 	  color: #fff !important;
 	}';
 
-	$css .= '.webnotik-pages .location-list li a {
+	$css .= '
+	.webnotik-pages .location-list li a {
 		color: '.$button_color.'
 	}';
 }
