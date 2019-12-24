a7bd304635bd24b6b0d95481738d5bc13a50911e
wpnimitz <wpnimitz@gmail.com>
2019-12-25 04:25:02 +0800

    Update toolbox.php

--- plugins/toolbox.php
+++ plugins/toolbox.php
@@ -1,6 +1,25 @@
 <?php
 $pages = array('General', 'Branding', 'Forms', 'City Pages', 'Divi Global', 'Help & Guidelines');
 
+add_action('admin_menu', 'toolbox_admin_menu_999');
+function toolbox_admin_menu_999() {
+    add_menu_page( __('Toolbox', 'rei-toolbox'), __('Toolbox', 'rei-toolbox'), 'manage_options', 'toolbox', 'admin_rei_toolbox_content', 'dashicons-flag', 3);
+    //call register settings function
+	add_action( 'admin_init', 'webnotik_register_forms_settings' );
+	add_action( 'admin_init', 'webnotik_register_general_settings' );
+	add_action( 'admin_init', 'webnotik_register_branding_settings' );
+	add_action( 'admin_init', 'webnotik_register_keywords_settings' );
+	add_action( 'admin_init', 'webnotik_register_topics_settings' );
+	add_action( 'admin_init', 'webnotik_register_divi_global_settings' );
+}
+function admin_rei_toolbox_content() {
+	$tab = isset($_GET["tab"]) ? $_GET["tab"] : 'general';
+
+
+	?>
+	<h1><?php echo $tab; ?></h1>
+
+<?php } //close admin_rei_toolbox_content
 add_action( 'wp_before_admin_bar_render', 'toolbox_admin_bar_render' );
 function toolbox_admin_bar_render() {
     global $wp_admin_bar;
@@ -23,7 +42,7 @@ function toolbox_admin_bar_render() {
 		    	'parent' => 'toolbox',
 		        'id' => 'toolbox-' . toolbox_create_slug($pages[$i]),
 		        'title' => __($pages[$i]),
-		        'href' => admin_url( 'admin.php?page=webnotik-real-estate&tab=' . toolbox_create_slug($pages[$i]) )
+		        'href' => admin_url( 'admin.php?page=toolbox&tab=' . toolbox_create_slug($pages[$i]) )
 		    )
 		);
     }
@@ -44,25 +63,3 @@ function toolbox_create_slug($string) {
     
     return $string;
 }
-
-
-
-add_action('admin_menu', 'toolbox_admin_menu_999');
-function toolbox_admin_menu_999() {
-    add_menu_page( __('Toolbox', 'rei-toolbox'), __('Toolbox', 'rei-toolbox'), 'manage_options', 'toolbox', 'admin_rei_toolbox_content', 'dashicons-flag', 3);
-    //call register settings function
-	add_action( 'admin_init', 'webnotik_register_forms_settings' );
-	add_action( 'admin_init', 'webnotik_register_general_settings' );
-	add_action( 'admin_init', 'webnotik_register_branding_settings' );
-	add_action( 'admin_init', 'webnotik_register_keywords_settings' );
-	add_action( 'admin_init', 'webnotik_register_topics_settings' );
-	add_action( 'admin_init', 'webnotik_register_divi_global_settings' );
-}
-function admin_rei_toolbox_content() {
-	$tab = isset($_GET["tab"]) ? $_GET["tab"] : 'general';
-
-
-	?>
-	<h1><?php echo $tab; ?></h1>
-
-<?php } //close admin_rei_toolbox_content
