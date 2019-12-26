52f28e7fe05069e5b7230b20a89919fe528f16a8
wpnimitz <wpnimitz@gmail.com>
2019-12-27 01:47:12 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
-include_once("plugins/webnotik.php");
-//include_once("plugins/toolbox.php");
+//include_once("plugins/webnotik.php");
+include_once("plugins/toolbox.php");
 include_once("plugins/shortcode.php");
 include_once("assets/other/comparison.php");
 
